730e1a99d11e996bfc18eab167f2f371ff163f0d
sergeymaguire <sergeymaguire@gmail.com>
2019-04-15 13:46:32 -0700

    A weather wind chill temperature  calculating windows application

--- WeatherCalculator/Form1.Designer.cs
+++ WeatherCalculator/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.inputValues = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // inputValues
+            // 
+            this.inputValues.Location = new System.Drawing.Point(41, 23);
+            this.inputValues.Name = "inputValues";
+            this.inputValues.Size = new System.Drawing.Size(204, 270);
+            this.inputValues.TabIndex = 0;
+            this.inputValues.TabStop = false;
+            this.inputValues.Text = "Input Values";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.inputValues);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox inputValues;
     }
 }
 
